81fd990b068bc25930f7b8c17c659420730d2c6e
tingkanghuang <102243634+tingkanghuang@users.noreply.github.com>
2022-09-22 11:38:32 +0800

    Add files via upload

---
+++ readvm.py
@@ -0,0 +1,26 @@
+data=[]
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count = count + 1
+		if count % 1000 == 0:
+			print(len(data))
+
+print('檔案讀取完畢，共有',len(data),'筆留言')
+
+sumlen = 0
+for d in data:
+	len(d)
+	sumlen = sumlen + len(d)
+
+print('平均留言字數長度":', sumlen/len(data))
+
+new =[]
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new),'筆資料字數小於100')
+print(new[0])
+
+
